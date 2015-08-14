6e62ec4194c0cfbb44c6156645d58e3d2c673999
Rodrigo de Blas <rodrigo.blas@autentia.com>
2015-08-14 11:05:57 +0200

    Creación de proyecto en Eclipse

---
+++ conseionariodb_schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE car_colours (
+    id serial PRIMARY KEY NOT NULL,
+    colour character varying(20) UNIQUE NOT NULL
+);
+
+
+CREATE TABLE cars (
+    id serial PRIMARY KEY NOT NULL,
+    brand character varying(20) NOT NULL,
+    model character varying(20) NOT NULL,
+    year integer  NOT NULL,
+    power integer NOT NULL,
+    colour character varying(20) REFERENCES car_colours(colour),
+    price numeric NOT NULL
+);
